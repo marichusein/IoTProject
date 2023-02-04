a91d07a56a17869e909ab20adf352cb05b74f0a6
DESKTOP-5DJOIPF\Husein <husein.maric@edu.fit.ba>
2023-02-05 00:15:25 +0100

    Firebase and fronend connected

--- frontend/index.js
+++ frontend/index.js
@@ -1,25 +1,60 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCL1YiihosH7HdGBgiA7suzHaK5IATjvYA",
+    authDomain: "iot-connectedhouse-ib200001.firebaseapp.com",
+    databaseURL: "https://iot-connectedhouse-ib200001-default-rtdb.firebaseio.com",
+    projectId: "iot-connectedhouse-ib200001",
+    storageBucket: "iot-connectedhouse-ib200001.appspot.com",
+    messagingSenderId: "794512223865",
+    appId: "1:794512223865:web:716d4c79f2675cba65eea8",
+    measurementId: "G-YYFMEPKELX"
+  };
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();  
+var firebaseRef=firebase.database().ref("svjetlo");
+var firebaseRef1=firebase.database().ref("prozor");
+
 function Svjetlo() {
     var dugme = document.getElementById("svjetlo");
     if (dugme.innerText === "Ugasi svjetlo") {
         dugme.innerText = "Upali svjetlo";
+        firebaseRef.set(false);
     }
     else {
         dugme.innerText = "Ugasi svjetlo";
+        firebaseRef.set(true);
+
     }
     // var test=document.getElementById("prozor");
     // alert(test.value);
 
 }
 var zakljucano=true;
+var slider=document.getElementById("prozor");
+slider.disabled=true;
+slider.value=0;
 function Zakljucaj(){
-    var test=document.getElementById("prozor");
+    var prozor=document.getElementById("prozor");
+    var dugme=document.getElementById("prozorZakljucaj");
     if(zakljucano==true){
-        
+       
+        slider.disabled=true;
+        prozorZakljucaj.innerText="Otključaj prozor";
     }
-    if(test.value==0){
+    else{
+        slider.disabled=false;
+        prozorZakljucaj.innerText="Zaključaj prozor";
+
+
+    }
+    if(prozor.value==0){
         zakljucano=!zakljucano;
     }
     else{
         alert("Prvo zatvorite prozor");
     }
+}
+function NagibProzora(){
+
+    firebaseRef1.set(slider.value);
+
 }
