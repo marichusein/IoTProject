3027f3329ad9b1e5cfacb736f4328854681d297a
DESKTOP-5DJOIPF\Husein <husein.maric@edu.fit.ba>
2023-02-07 11:17:19 +0100

    Finish NodeMCU code and frontend

--- frontend/index.js
+++ frontend/index.js
@@ -7,66 +7,92 @@ const firebaseConfig = {
     messagingSenderId: "794512223865",
     appId: "1:794512223865:web:716d4c79f2675cba65eea8",
     measurementId: "G-YYFMEPKELX"
-  };
+};
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();  
-var firebaseRef=firebase.database().ref("svjetlo");
-var firebaseRef1=firebase.database().ref("prozor");
-var firebaseRef2=firebase.database().ref("pozar");
+firebase.analytics();
+var firebaseRef = firebase.database().ref("svjetlo");
+var firebaseRef1 = firebase.database().ref("prozor");
+var firebaseRef2 = firebase.database().ref("pozar");
+var kodKuceAktivno = firebase.database().ref("aktivno");
+var detektovan = firebase.database().ref("detektovano");
+
+
 
 const database1 = firebase.database();
-setInterval(function() {
+setInterval(function () {
     const ref = database1.ref("pozar");
-    ref.once("value", function(snapshot) {
-      console.log(snapshot.val());
-      if(snapshot.val()==true){
-        alert("Požar detektovan");
-        firebaseRef2.set(false);
-      }
+    ref.once("value", function (snapshot) {
+        console.log(snapshot.val());
+        if (snapshot.val() == true) {
+            alert("Požar detektovan");
+            var pozarF = document.getElementById("pokret");
+            pozarF.innerText = "Goriii";
+            firebaseRef2.set(false);
+        }
     });
-  }, 2000);
+}, 2000);
+setInterval(function () {
+    const ref = database1.ref("detektovano");
+    ref.once("value", function (snapshot) {
+        if (snapshot.val() == true) {
+            alert("Neko je u kući");
+            detektovan.set(false);
+        }
+    });
+}, 2000);
 function Svjetlo() {
     var dugme = document.getElementById("svjetlo");
     if (dugme.innerText === "Ugasi svjetlo") {
         dugme.innerText = "Upali svjetlo";
         firebaseRef.set(false);
+
+
     }
     else {
         dugme.innerText = "Ugasi svjetlo";
         firebaseRef.set(true);
+
+
 
     }
     // var test=document.getElementById("prozor");
     // alert(test.value);
 
 }
-var zakljucano=true;
-var slider=document.getElementById("prozor");
-slider.disabled=true;
-slider.value=0;
-function Zakljucaj(){
-    var prozor=document.getElementById("prozor");
-    var dugme=document.getElementById("prozorZakljucaj");
-    if(zakljucano==true){
-       
-        slider.disabled=true;
-        prozorZakljucaj.innerText="Otključaj prozor";
+var zakljucano = true;
+var slider = document.getElementById("prozor");
+slider.disabled = true;
+slider.value = 0;
+function Zakljucaj() {
+    var prozor = document.getElementById("prozor");
+    var dugme = document.getElementById("prozorZakljucaj");
+    if (zakljucano == true) {
+
+        slider.disabled = true;
+        prozorZakljucaj.innerText = "Otključaj prozor";
     }
-    else{
-        slider.disabled=false;
-        prozorZakljucaj.innerText="Zaključaj prozor";
+    else {
+        slider.disabled = false;
+        prozorZakljucaj.innerText = "Zaključaj prozor";
 
 
     }
-    if(prozor.value==0){
-        zakljucano=!zakljucano;
+    if (prozor.value == 0) {
+        zakljucano = !zakljucano;
     }
-    else{
+    else {
         alert("Prvo zatvorite prozor");
     }
 }
-function NagibProzora(){
+function NagibProzora() {
 
-    firebaseRef1.set(slider.value);
 
+    firebaseRef1.set(parseInt(slider.value));
+
+}
+function KodKuce() {
+    kodKuceAktivno.set(false);
+}
+function NismoKodKuce() {
+    kodKuceAktivno.set(true);
 }
